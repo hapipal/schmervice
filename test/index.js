5ca62f412def395ce6a685f3617cfa8dc8008ab8
Devin Ivy <devin@bigroomstudios.com>
2017-11-20 00:12:05 -0500

    Complete initial tests of Service class

--- test/index.js
+++ test/index.js
@@ -203,6 +203,93 @@ describe('Schmervice', () => {
 
             expect(() => new ServiceX(server, {})).to.throw('Caching config can only be specified once.');
         });
+
+        it('accepts caching config in form { cache, generateTimeout }.', async () => {
+
+            const sleep = (ms) => {
+
+                return new Promise((resolve) => setTimeout(resolve, ms));
+            };
+
+            const ServiceX = class ServiceX extends Schmervice.Service {
+
+                async add(a, b, fail) {
+
+                    this.called = (this.called || 0) + 1;
+
+                    if (fail) {
+                        await sleep(3);
+                    }
+
+                    return a + b;
+                }
+            };
+
+            ServiceX.caching = {
+                add: {
+                    cache: {
+                        expiresIn: 100,
+                        generateTimeout: 2
+                    },
+                    generateKey: (a, b) => {
+
+                        // Addition is commutative
+                        return (a < b) ? `${a}:${b}` : `${b}:${a}`;
+                    }
+                }
+            };
+
+            const server = Hapi.server();
+            const serviceX = new ServiceX(server, {});
+
+            await server.initialize();
+
+            expect(serviceX.called).to.not.exist();
+
+            // Check { generateKey }
+            await serviceX.add(1, 2, false);
+            expect(serviceX.called).to.equal(1);
+
+            await serviceX.add(2, 1, false);
+            expect(serviceX.called).to.equal(1);
+
+            await serviceX.add(1, 3, false);
+            expect(serviceX.called).to.equal(2);
+
+            // Check { cache }
+            await expect(serviceX.add(1, 4, true)).to.reject('Service Unavailable');
+        });
+
+        it('accepts caching config in form { ...cache }.', async () => {
+
+            const sleep = (ms) => {
+
+                return new Promise((resolve) => setTimeout(resolve, ms));
+            };
+
+            const ServiceX = class ServiceX extends Schmervice.Service {
+
+                async add(a, b, fail) {
+
+                    await sleep(3);
+
+                    return a + b;
+                }
+            };
+
+            ServiceX.caching = {
+                add: {
+                    generateTimeout: 2
+                }
+            };
+
+            const server = Hapi.server();
+            const serviceX = new ServiceX(server, {});
+
+            await server.initialize();
+
+            await expect(serviceX.add(1, 2)).to.reject('Service Unavailable');
+        });
     });
 
     describe('plugin', () => null);
